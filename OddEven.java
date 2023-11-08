e7c8bb15dd51187e39565990f462c23ecb453b8c
Valarmathi G <141111982+Valarmathi8@users.noreply.github.com>
2023-11-08 12:14:11 +0530

    OddEven Task

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+class OddEven
+{
+	public static void main(String ags[])
+	{
+		for( int i=1;i<=30;i++)
+		{
+		if(i % 2==0)
+		{
+			System.out.println(i + " is an Even number");
+		}
+		else
+		{
+			System.out.println(i + " is an Odd number");
+		}
+		}
+
+	}
+}
